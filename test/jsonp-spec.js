c82c7d1d309ebed315d5765d28947e708c0605ad
Afonso <noreply>
2014-05-03 23:52:43 +0000

    Update component /test/jsonp-spec.js

--- test/jsonp-spec.js
+++ test/jsonp-spec.js
@@ -1,16 +1,10 @@
-/**
- * @module test/jsonp-spec
- * @requires montage/core/core
- */
-var Montage = require("montage/core/core").Montage;
-/**
- * @class JsonpSpec
- * @extends Montage
- */
-exports.JsonpSpec = Montage.specialize(/** @lends JsonpSpec# */ {
-    constructor: {
-        value: function JsonpSpec() {
-            this.super();
-        }
-    }
-});
+var Jsonp = require("core/jsonp").Jsonp;
+
+describe("jsonp-spec", function() {
+    it("should get an object from a jsonp endpoint", function() {
+        return Jsonp.makeRequest("http://echo.jsontest.com/key/value/one/two")
+        .then(function(response) {
+            expect(response).toEqual({key: "value", one: "two"})
+        }).timeout(5000);
+    });
+})
